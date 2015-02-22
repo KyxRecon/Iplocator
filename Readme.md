1a30c6df0a1bd379fa483332333f60d6e308b825
root <root@localhost.localdomain>
2015-02-21 19:10:44 -0600

    iplocator-tool

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# ip locator : is a simple tool coded in perl to help you to identify sites or victims geographical location
+without login in the browser and search about & it capable find, i.e. country, region, city, latitude, longitude, ZIP code, time zone, connection speed, ISP, domain name, IDD country code, area code, weather station code and name, mobile carrier, elevation and usage type information using a proprietary IP address lookup database and technology without invading the Internet user's privacy. 
+
+-------------
+Vectims IP : 
+-------------
+
+83.66.210.106
+72.14.199.174
+69.171.247.119
+172.31.132.49
+116.73.205.230
+59.178.206.168
+92.40.76.244
+223.234.23.42
+223.234.23.42
+
+--------------
+Sites :
+--------------
+
+www.bloomberg.com
+www.cnet.com
+comnews.com
+www.datamation.com
+time.com
+www.zdnet.com
+www.siia.net
+
+
+simple to Run , perl iplocator.Ex
---
+++ iplocator.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+# Ip Geolocation 
+#By Alexcerus
+# API documentation - http://ip-api.com/docs/
+# you can see you external ip using this Bash script :
+# read COUNTRY IP <<< $(wget -qO- http://ip-api.com/line/?fields=countryCode,query); echo $IP $COUNTRY
+# Now let's start :
+
+use Socket;
+use Term::ANSIColor;
+use WWW::Mechanize;
+use JSON;
+  
+ 
+print color 'bold bright_yellow';
+ 
+print q{
+
+        ~o88ooooooooooooo88o~
+   ouooooo,~oo         oo~,ooooouo
+   8     ~88888.     ,88888~     8
+   8      go~~~os   go~~~os      8
+   8    ,8`     '8_8`     '8.    8
+   8    8`   _   '8`   _   '8    8
+   8    8   !@!   8   !@!   8    8
+   8    8i       /8\       i8    8
+   8     8s     g8 8s     s8     8
+   8      dooooo`8_8'ooooob      8
+   8     d!      'V`      !b     8
+   8     8        ~        8     8
+   8     8                 8     8	
+   8   ] 8                 8 [   8 
+   8 [ ] 8                 8 [ ] 8
+   8 [ ] !8               8| [ ] 8
+   8 [ ]s88b-oo- xxx -oo-d88s[ ] 8
+   8 [,88  8i'`   ~   '`i8  88.] 8
+   8 88`   88s'88` '88`gf8   '88 8
+   888   ,g8s/8. ooo ,8\g8s.   888
+   88`  i888888fo_X_of888888i  '88
+   V    YY'`~'`  ~~~  '` ~ YY    V
+        ""                 ""
+
+Ip Geolocation Tool  
+By : Alexcerus-HR 
+
+------------------------------------
+};
+ 
+print color 'reset';
+@iphost=$ARGV[0] || die "Usage : ./Iplocation.pl [host] [ip] [domain] \n\nEx:  ./Iplocation.pl  www.google.com \n     ./Iplocation.pl  216.58.210.206\n \n";
+my @ip = inet_ntoa(scalar gethostbyname("@iphost")or die "IP or Host invalid!\n");
+my @hn = scalar gethostbyaddr(inet_aton(@ip),AF_INET);
+ 
+my $GET=WWW::Mechanize->new();
+    $GET->get("http://ip-api.com/json/@ip"); # JSON API OF IP-API.COM
+    my $json = $GET->content();
+ 
+ 
+my $info = decode_json($json);
+# Json API Response :
+# A successful request will return, by default, the following:
+#{
+#    "status": "success",
+#    "country": "COUNTRY",
+#    "countryCode": "COUNTRY CODE",
+#    "region": "REGION CODE",
+#    "regionName": "REGION NAME",
+#    "city": "CITY",
+#    "zip": "ZIP CODE",
+#    "lat": LATITUDE,
+#    "lon": LONGITUDE,
+#    "timezone": "TIME ZONE",
+#    "isp": "ISP NAME",
+#    "org": "ORGANIZATION NAME",
+#    "as": "AS NUMBER / NAME",
+#   "query": "IP ADDRESS USED FOR QUERY"
+# }
+# INFOS OF JSON API ...
+ 
+print "  [!] IP: ", $info->{'query'}, "\n";
+print "------------------------------------\n"; 
+print "  [+] ORG: ", $info->{'as'}, "\n";
+print "  [+] ISP: ", $info->{'isp'}, "\n";
+print "  [+] Country: ", $info->{'country'}," - ", $info->{'countryCode'}, "\n";
+print "  [+] City: ", $info->{'city'}, "\n";
+print "  [+] Region: ", $info->{'regionName'}, " - " , $info->{'region'}, "\n";
+print "  [+] Geo: ", "Lat: " , $info->{'lat'}, " - Long: ", $info->{'lon'}, "\n";
+print "  [+] Geo: ", "Latitude: " , $info->{'lat'}, " - Long: ", $info->{'lat'}, "\n";
+print "  [+] Time: ", "timezone: " , $info->{'timezone'}, " - Long: ", $info->{'timezone'}, "\n";
+print "  [+] As number/name: ", "as: " , $info->{'as'}, " - Long: ", $info->{'as'}, "\n";
+print "  [+] ORG: ", $info->{'as'}, "\n";
+print "  [+] Country code: ", $info->{'countryCode'}, "\n"; 
+print "  [+] Status: ", $info->{'status'}, "\n"; 
+print "\n";
+# EOF
